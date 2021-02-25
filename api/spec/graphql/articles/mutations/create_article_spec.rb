c50d7abf0586d35e0ecda7c0d1abe4861a3de65a
erkizie <erkizie@gmail.com>
2021-02-25 23:58:52 +0200

    Mutation tests for CreateArticle mutation

---
+++ api/spec/graphql/articles/mutations/create_article_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+require_relative '../../../support/graphql_spec_helper'
+include GraphQL::TestHelpers
+
+describe 'CreateArticle', type: :mutation do
+  describe 'Create an Article' do
+
+    let(:user) { create(:user) }
+    let(:mutation_type) { "createArticle" }
+    let(:mutation_string) { <<-GQL
+        mutation createArticle($input: CreateArticleInput!) {
+          createArticle(input: $input) {
+            article {
+                id
+                title
+                description
+                body
+                userId
+            }
+          }
+        }
+    GQL
+    }
+
+    context 'valid input for article' do
+
+      before do
+        mutation(
+          mutation_string,
+          variables: {
+            input: {
+              title: "Test title",
+              description: "Test description",
+              body: "Test body"
+            }
+          },
+          context: {
+            current_user: user
+          }
+        )
+      end
+
+      it 'returns no errors' do
+        expect(gql_response.errors).to be_nil
+      end
+
+      it 'has successful result' do
+        expect(gql_response.data[mutation_type]["article"]).to include("title" => "Test title", "description" => "Test description", "body" => "Test body", "userId" => user.id.to_s)
+      end
+    end
+
+    context 'invalid input for article' do
+
+      before do
+        mutation(
+          mutation_string,
+          variables: {
+            input: {
+              description: "Test description",
+              body: "Test body"
+            }
+          },
+          context: {
+            current_user: user
+          }
+        )
+      end
+
+      it 'returns an error' do
+        expect(gql_response.errors[0]["message"]).to include("Expected value to not be null")
+      end
+
+      it "doesn't return the article object" do
+        expect(gql_response.data).to be_nil
+      end
+    end
+  end
+end
