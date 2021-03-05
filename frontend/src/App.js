bfe21afb10ba89143da34656e64ec8dab0c5d935
erkizie <erkizie@gmail.com>
2021-03-06 00:01:14 +0200

    Setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+      <div className="wrapper">
+        <p>Hello!</p>
+        <h1>Bye</h1>
+    </div>
+  );
+}
+
+export default App;
