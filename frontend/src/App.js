ecddcc2251cdeb2237fea24f14e91afdbf8f86f3
erkizie <erkizie@gmail.com>
2021-03-08 20:58:35 +0200

    Add mdbootstrap and router. CleanUp main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TestComp from './components/TestComp';
 import SignUpContainer from './components/user/sign_up/SignUpContainer';
+
 function App() {
   return (
     <div className="wrapper">
