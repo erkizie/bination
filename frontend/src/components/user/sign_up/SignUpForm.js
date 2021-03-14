8952617153b05d12f80a09471420aef5b4ff0e96
erkizie <erkizie@gmail.com>
2021-03-14 19:41:35 +0200

    Update form. Validation using mdbootstrap styles. PW strength ui.

--- frontend/src/components/user/sign_up/SignUpForm.js
+++ frontend/src/components/user/sign_up/SignUpForm.js
@@ -1,36 +1,100 @@
 import React from 'react';
 import './style.css';
-import PasswordStr from "./PasswordStr";
-import { Link as ReactLink } from 'react-router-dom';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
-
+import PasswordStr from './PasswordStr';
+import {Link as ReactLink} from 'react-router-dom';
+import {MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput} from 'mdbreact';
 
 const SignUpForm = ({
+                      onSubmit,
+                      onChange,
+                      errors,
+                      user,
+                      score,
+                      btnTxt,
+                      type,
+                      pwMask,
+                      onPwChange,
                     }) => {
   return (
       <MDBContainer>
         <MDBRow className="d-flex justify-content-center">
           <MDBCol md="4">
-            <form>
+            <form onSubmit={onSubmit}>
               <p className="h5 text-center mb-4">Sign up</p>
               <div className="grey-text">
-                <MDBInput label="Your username"
+                <MDBInput name="username"
+                          className="inputElement"
+                          label="Your username"
                           icon="user"
-                          group type="text"
-                          validate error="wrong"
-                          success="right" />
-                <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
-                          success="right" />
-                <MDBInput label="Your password" icon="lock" group type="password" validate />
-                <MDBInput label="Confirm password" icon="exclamation-triangle" group type="password" validate />
+                          type="text"
+                          value={user.username}
+                          onChange={onChange}>
+                  <div className="invalid-feedback">
+                    {errors.username}
+                  </div>
+                  <div className="valid-feedback">Looks good!</div>
+                </MDBInput>
+                <MDBInput name="email"
+                          className="inputElement"
+                          label="Your email"
+                          icon="envelope"
+                          type="email"
+                          value={user.email}
+                          onChange={onChange}>
+                  <div className="invalid-feedback">
+                    {errors.email}
+                  </div>
+                  <div className="valid-feedback">Looks good!</div>
+                </MDBInput>
+                <MDBInput type={type}
+                          name="password"
+                          className="inputElement"
+                          label="Your password"
+                          icon="lock"
+                          value={user.password}
+                          onChange={onPwChange}>
+                  <div className="invalid-feedback">
+                    {errors.password}
+                  </div>
+                  <div className="valid-feedback">Looks good!</div>
+                </MDBInput>
+
+                <div className="pwStrRow">
+                  {score >= 1 && (
+                      <div>
+                        <PasswordStr score={score}/>
+                        <MDBBtn
+                            className="btn btn-sm"
+                            onClick={pwMask}
+                            style={{
+                              position: 'relative',
+                              left: '50%',
+                              transform: 'translateX(-50%)',
+                            }}
+                        >{btnTxt}</MDBBtn>
+                      </div>
+                  )}
+                </div>
+                <MDBInput type={type}
+                          name="pwconfirm"
+                          className="inputElement"
+                          label="Confirm password"
+                          icon="exclamation-triangle"
+                          value={user.pwconfirm}
+                          onChange={onChange}>
+                  <div className="invalid-feedback">
+                    {errors.pwconfirm}
+                  </div>
+                  <div className="valid-feedback">Looks good!</div>
+                </MDBInput>
 
               </div>
               <div className="text-center">
-                <MDBBtn color="primary">Register</MDBBtn>
+                <MDBBtn color="primary" type="submit">Register</MDBBtn>
               </div>
             </form>
             <p className="loginBox">
-              Aleady have an account? <br />
+              Aleady have an account? <br/>
               <ReactLink to="/sign_in">Log in here</ReactLink>
             </p>
           </MDBCol>
